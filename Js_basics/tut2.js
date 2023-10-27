9578e69778c7de3ef767cde54b7e6088b0c380ed
PRITI GUPTA <125783687+pritigupta118@users.noreply.github.com>
2023-10-27 15:24:19 +0000

    datatypes

---
+++ Js_basics/tut2.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "pritigupta1144@gmail.com"
+var accountPassword = "12345"
+
+let accountState
+accountCity = "Westbengal"
+
+// accountId = 2
+accountEmail = "prachigupta1144@gmail.com"
+accountPassword = "66534"
+accountCity = "London"
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
